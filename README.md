382dc7a98679377a1240156baa93b26ba4a8026a
vinaykudari <svvk.20140513@btech.nitdgp.ac.in>
2017-12-29 03:05:46 +0530

    Added README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BandwidthStability
-Predict bandwidth 
+Bandwidth prediction using machine learning alogrithm
+## Getting Started
+First we need to collect network bandwidth data, then apply linear regression - gradient descent alogrithm to predict.
--- get_data.py
+++ get_data.py
@@ -1,28 +1,27 @@
 import speedtest
 import datetime
-from tinydb import TinyDB,Query
+from tinydb import TinyDB
 
 time = datetime.datetime.now()
 db = TinyDB('bandwidth_data.json')
 
-month = time.month
-day = time.day
-hour = time.strftime("%H")
-weekday = time.weekday()
-server = [12353]
+month = int(time.month)
+day = int(time.day)
+hour = int(time.strftime("%H")) + 1                             #To avoid 0
+weekday = int(time.weekday()) + 1
+server = [12353]                                                #Best server for BSNL Kolkata Region
 download_speed = 0
-
-print(month)
-print(day)
-print(hour+1)
-print(weekday)
 
 s = speedtest.Speedtest()
 s.get_servers(server)
 s.get_best_server()
 
-for i in range(0,3):
+for i in range(0,3):                                            #Averaging to get optimum bandwidth
     download_speed += s.download()
-    print(download_speed)
 
 download_speed = download_speed/3
+
+db.insert({'month':month,"day":day,"weekday":weekday,"hour":hour,"download_speed":download_speed})
+
+
+
